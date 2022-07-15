73c0a00707b432acdb09184b965c2b962b84d50f
FireLord <firelord.xda@gmail.com>
2022-07-15 12:08:12 +0530

    first
    Signed-off-by: FireLord <firelord.xda@gmail.com>

---
+++ app/src/main/java/com/firelord/notificationdemo1/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.firelord.notificationdemo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            displayNotification()
+        }
+    }
+
+    private fun displayNotification(){
+
+    }
+}
