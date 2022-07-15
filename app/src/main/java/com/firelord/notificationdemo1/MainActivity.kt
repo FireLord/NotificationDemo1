d8cb6d10fb380b35f41af351f6e6da252fc34a33
FireLord <firelord.xda@gmail.com>
2022-07-15 12:55:19 +0530

    open activity when notification is clicked
    Signed-off-by: FireLord <firelord.xda@gmail.com>

--- app/src/main/java/com/firelord/notificationdemo1/MainActivity.kt
+++ app/src/main/java/com/firelord/notificationdemo1/MainActivity.kt
@@ -3,7 +3,9 @@ package com.firelord.notificationdemo1
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -31,12 +33,21 @@ class MainActivity : AppCompatActivity() {
 
     private fun displayNotification(){
         val notificationId = 45
+        val tapResultIntent = Intent(this,SecondActivity::class.java)
+        val pendingIntent:PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            tapResultIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
         val notification = NotificationCompat.Builder(this@MainActivity,channelID)
             .setContentTitle("Demo Title")
             .setContentText("This is demo notification")
             .setSmallIcon(android.R.drawable.ic_dialog_info)
             .setAutoCancel(true)
             .setPriority(Notification.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
             .build()
         notificationManager?.notify(notificationId,notification)
     }
